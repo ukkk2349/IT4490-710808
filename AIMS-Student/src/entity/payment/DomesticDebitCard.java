d34733ed501aec12f8f40b5fe1fba7a793f11709
ukkk2349 <uxumituot@gmail.com>
2021-12-26 20:25:55 +0700

    add week13

--- AIMS-Student/src/entity/payment/DomesticDebitCard.java
+++ AIMS-Student/src/entity/payment/DomesticDebitCard.java
@@ -1,12 +1,12 @@
 package entity.payment;
 
-public class DebitCard {
+public class DomesticDebitCard {
 	private String issuingBank;
 	private String cardNumber;
 	private String validFromDate;
 	private String cardholderName;
 	
-	public DebitCard(String issuingBank, String cardNumber, String validFromDate, String cardholderName) {
+	public DomesticDebitCard(String issuingBank, String cardNumber, String validFromDate, String cardholderName) {
 		super();
 		this.issuingBank = issuingBank;
 		this.cardNumber = cardNumber;
